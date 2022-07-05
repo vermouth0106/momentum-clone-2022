6543a8329311e15169cc52233cea897d26bd7ffd
bnetcoder <silr01@naver.com>
2022-07-05 16:08:06 +0900

    Create weather.js

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "0a9c74fdbdeec3088202eb4731e24274";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((respone) => respone.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
